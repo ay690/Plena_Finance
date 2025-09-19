6f47f587b5946738d7d6d2a9ad00437aa5803c49
Aniket Yadav <aniketyadav690@gmail.com>
2025-09-20 00:21:30 +0530

    UI: Portfolio's Header

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,10 @@
-
 import type { JSX } from "react";
+import { PortfolioSection } from "./Home/sections/PortfolioSection/PortfolioSection";
 
 export const Home = (): JSX.Element => {
   return (
     <main className="w-full p-4 mx-auto min-h-screen flex flex-col bg-darkbackgroundsbg-base">
-      <h1 className="text-xl text-center text-white flex items-center justify-center min-h-screen">Hello</h1>
+      <PortfolioSection />
     </main>
   );
 };
---
+++ src/screens/Home/sections/PortfolioSection/PortfolioSection.tsx
@@ -0,0 +1,32 @@
+import { Button } from "../../../../components/ui/button";
+import type { JSX } from "react/jsx-dev-runtime";
+import logo from "../../../../assets/Logo.png";
+import walletIcon from "../../../../assets/wallet.png";
+
+export const PortfolioSection = (): JSX.Element => {
+  return (
+    <div className="flex w-full h-14 relative items-center gap-1.5 p-3">
+      <div className="flex items-center gap-3 relative flex-1 grow">
+        <img className="relative flex-[0_0_auto]" alt="Logo" src={logo} />
+
+        <div className="relative flex-1 [font-family:'Inter',Helvetica] font-semibold text-white text-xl tracking-[0] leading-6">
+          Token Portfolio
+        </div>
+      </div>
+
+      <div className="inline-flex items-center gap-6 relative flex-[0_0_auto]">
+        <Button className="px-2.5 py-1.5 bg-[#a9e851] rounded-[100px] shadow-[0px_0px_0px_1px_#1f6619,0px_1px_2px_#1f661966,inset_0px_0.75px_0px_#ffffff33] inline-flex items-center justify-center gap-1.5 relative flex-[0_0_auto] overflow-hidden h-auto hover:bg-[#98d147] cursor-pointer">
+          <img
+            className="relative flex-[0_0_auto]"
+            alt="Wallet"
+            src={walletIcon}
+          />
+
+          <div className="mt-[-1.00px] text-black/70 font-medium">
+            Connect WalletIcon
+          </div>
+        </Button>
+      </div>
+    </div>
+  );
+};
