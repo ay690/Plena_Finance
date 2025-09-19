dbdc775f2accdfc77c25e14a737301046b6591e6
Aniket Yadav <aniketyadav690@gmail.com>
2025-09-19 21:34:55 +0530

    initialized and assests added and layout created

---
+++ src/screens/Home.tsx
@@ -0,0 +1,10 @@
+
+import type { JSX } from "react";
+
+export const Home = (): JSX.Element => {
+  return (
+    <main className="w-full p-4 mx-auto min-h-screen flex flex-col bg-darkbackgroundsbg-base">
+      <h1 className="text-xl text-center text-white flex items-center justify-center min-h-screen">Hello</h1>
+    </main>
+  );
+};
