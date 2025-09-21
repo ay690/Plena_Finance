29931c600b8f5bfc5f5498d913760cd485b5589d
Aniket Yadav <aniketyadav690@gmail.com>
2025-09-21 18:11:19 +0530

    feat: Lazy loading

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,21 @@
 import type { JSX } from "react";
-import { WatchlistSection } from "./Home/sections/WatchlistSection/WatchlistSection";
+import React, { Suspense } from "react";
 import { PortfolioSection } from "./Home/sections/PortfolioSection/PortfolioSection";
+import Spinner from "@/components/spinner";
+
+const WatchlistSection = React.lazy(() =>
+  import("./Home/sections/WatchlistSection/WatchlistSection").then((m) => ({
+    default: m.WatchlistSection,
+  }))
+);
 
 export const Home = (): JSX.Element => {
   return (
     <main className="w-full p-10 mx-auto min-h-screen flex flex-col bg-darkbackgroundsbg-base">
       <PortfolioSection />
-      <WatchlistSection />
+      <Suspense fallback={<Spinner />}>
+        <WatchlistSection />
+      </Suspense>
     </main>
   );
 };
